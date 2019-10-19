b7528449e52d39b4260ea7f0e0acc294ce1678a2
Bruno Serrão <brunosserrao@gmail.com>
2019-10-20 00:56:33 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+const onPress = () => {
+  Alert.alert('Hello World','Yes, Hello World!');
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={onPress} title="Hello World" />
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
